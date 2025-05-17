11c6bd694e8688c3bfa71d80f011bd6645959bc4
OadHarsh <harshoad7205@gmail.com>
2025-05-17 08:05:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.74%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def sortColors(self, arr):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+        return arr.sort()
