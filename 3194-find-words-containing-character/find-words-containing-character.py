3ff4825d35c7f2555d23284e6cc4e63c7aa5886c
OadHarsh <harshoad7205@gmail.com>
2025-05-24 23:01:10 +0530

    Time: 12 ms (5.58%) | Memory: 12.6 MB (0.52%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        """
+        :type words: List[str]
+        :type x: str
+        :rtype: List[int]
+        """
+
+        indices  = [];
+
+        for i in range(0,len(words)):
+            for j in words[i]:
+                if (x == j):
+                    indices.append(i);
+                    break;
+        return indices;            
