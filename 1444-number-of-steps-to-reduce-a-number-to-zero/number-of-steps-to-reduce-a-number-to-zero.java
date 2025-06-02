c51e0f7682562ed50f8642727c5d9cfe899b7c0d
OadHarsh <harshoad7205@gmail.com>
2025-06-02 22:55:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (40.82%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -3,11 +3,9 @@ class Solution {
         if(num ==0){
             return 0;
         }
-
         if(num % 2 == 0){
             return 1 + numberOfSteps(num/2);
         }
-
         return 1 + numberOfSteps(num-1);
     }
 }
